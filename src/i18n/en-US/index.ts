a024c52092c91a010558cbefb0b8c0733f628ffd
Nurjadein <nurjadeinabdulmorid@gmail.com>
2025-11-22 16:46:39 +0800

    english lang

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -1,7 +1,17 @@
-// This is just an example,
-// so you can safely delete all default props below
-
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful'
+  //login
+  'welcome-label': 'Welcome!',
+  'login-label': 'Login',
+  'please-login':'Please login to your account',
+  'username':'Username',
+  'password':'Password',
+  'password-is-required':'Password is Required!',
+  'username-is-required':'Username is Required!',
+  'language':'Language',
+  'english':'English',
+  'arabic':'Arabic',
+  'en-US':'en-US',
+  'ar-SA':'ar-SA'
+  
+
 };
