46f120dfb91c5473d1fd60ce7931f1cf5554f88f
Nurjadein <nurjadeinabdulmorid@gmail.com>
2025-11-22 16:46:31 +0800

    arabic lang

---
+++ src/i18n/ar-SA/index.ts
@@ -0,0 +1,15 @@
+export default {
+    //login
+  'welcome-label': 'مرحبا!',
+  'login-label': 'تسجيل الدخول',
+  'please-login':'يرجى تسجيل الدخول إلى حسابك',
+  'username':'اسم المستخدم',
+  'password':'كلمة المرور',
+  'password-is-required':'كلمة المرور مطلوبة!',
+  'username-is-required':'اسم المستخدم مطلوب!',
+  'language': 'لغة',
+  'arabic':'العربية',
+  'english':'الإنجليزية',
+  'en-US':'en-US',
+  'ar-SA':'ar-SA',
+};
